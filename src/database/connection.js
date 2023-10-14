ef89c135b63c854965aebbb869dc929b2b75c15f
Nox <gabrismar333@gmail.com>
2023-10-14 18:26:58 -0400

    "Fixed env variable name"

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,7 @@
 import pg from "pg"
 
 const db = new pg.Pool({
-    database: process.env.DB_HOST,
+    database: process.env.DB_NAME,
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD
