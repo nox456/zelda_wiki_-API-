62af12da580afc1b6b3fa9d5ee68c5bfcc5f6026
Nox <gabrismar333@gmail.com>
2023-10-15 13:19:33 -0400

    "Fixed CORS error"

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import morgan from "morgan";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 
-import characterRoutes from "./routes/characterRoutes.js"
-import gameRoutes from "./routes/gameRoutes.js"
-import raceRoutes from "./routes/raceRoutes.js"
-import consoleRoutes from "./routes/consoleRoutes.js"
-import enemieRoutes from "./routes/enemieRoutes.js"
+import characterRoutes from "./routes/characterRoutes.js";
+import gameRoutes from "./routes/gameRoutes.js";
+import raceRoutes from "./routes/raceRoutes.js";
+import consoleRoutes from "./routes/consoleRoutes.js";
+import enemieRoutes from "./routes/enemieRoutes.js";
 
 console.clear();
 const app = express();
@@ -21,14 +21,26 @@ app.set("port", process.env.PORT);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(join(__dirname,'public')))
+app.use(express.static(join(__dirname, "public")));
 
+const accepted_origins = [
+    "http://192.168.1.7:3000",
+    "http://zeldawikiserver-production.up.railway.app",
+];
 // Routes
-app.use("/characters", characterRoutes)
-app.use("/games", gameRoutes)
-app.use("/races", raceRoutes)
-app.use("/consoles", consoleRoutes)
-app.use("/enemies", enemieRoutes)
+app.use((req, res, next) => {
+    const origin = req.header("origin")
+    console.log(origin)
+    if (accepted_origins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin",origin)
+    }
+    next();
+});
+app.use("/characters", characterRoutes);
+app.use("/games", gameRoutes);
+app.use("/races", raceRoutes);
+app.use("/consoles", consoleRoutes);
+app.use("/enemies", enemieRoutes);
 
 // Server ON
 app.listen(app.get("port"), () => {
