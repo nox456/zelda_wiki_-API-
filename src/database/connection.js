1abae339b729e6f3568fb12fb4db79d867c20fa7
Nox <nox030705@gmail.com>
2024-01-09 01:18:02 -0400

    "Enemies json data"

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,6 +3,7 @@ import insert_consoles_data from "./data/consoles.js"
 import insert_games_data from "./data/games.js"
 import insert_races_data from "./data/races.js"
 import insert_characters_data from "./data/characters.js"
+import insert_enemies_data from "./data/enemies.js"
 
 const db = new pg.Pool({
     database: process.env.DB_NAME,
@@ -18,3 +19,4 @@ await insert_consoles_data()
 await insert_games_data()
 await insert_races_data()
 await insert_characters_data()
+await insert_enemies_data()
